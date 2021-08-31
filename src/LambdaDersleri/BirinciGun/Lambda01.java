37c20e3182227de364081a37a72536028646d2f8
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-08-31 17:08:16 +0200

    Lambda başslangıc

---
+++ src/LambdaDersleri/BirinciGun/Lambda01.java
@@ -0,0 +1,85 @@
+package LambdaDersleri.BirinciGun;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lambda01 {
+
+  public static void main(String[] args) {
+    List<Integer> l = new ArrayList<>(
+      Arrays.asList(14, 9, 13, 4, 9, 2, 4, 14, 15)
+    );
+    yazdirJava(l);
+    System.out.println();
+    yazdirLambda(l);
+    System.out.println();
+    ciftSayiJava(l);
+    System.out.println();
+    ciftSayiLambda(l);
+    System.out.println();
+    teksayiKare(l);
+    System.out.println();
+    farkliTekKup(l);
+    System.out.println();
+    ciftSayiKareToplam(l);
+    System.out.println();
+  }
+
+  //5) Lİstedeki tekrarlı elemanları silip çift sayı olanların karelerinin toplamını yazdıran metot oluştur
+
+  public static void ciftSayiKareToplam(List<Integer> l) {
+    System.out.print(
+      l
+        .stream()
+        .distinct()
+        .filter(t -> t % 2 == 0)
+        .map(t -> t * t)
+        .reduce(0, (x, y) -> x + y)
+    );
+  }
+
+  ///4)Listedeki tekrarlı elemanları silip tek sayı olanların küplerini yazdıran metod oluştur
+  public static void farkliTekKup(List<Integer> l) {
+    l
+      .stream()
+      .distinct()
+      .filter(t -> t % 2 == 1)
+      .map(t -> t * t * t)
+      .forEach(t -> System.out.print(t + " "));
+  }
+
+  //3) List teki tek sayıların karelerini yazdıran metod oluşturun
+
+  public static void teksayiKare(List<Integer> l) {
+    l
+      .stream()
+      .filter(t -> t % 2 == 1)
+      .map(t -> t * t)
+      .forEach(t -> System.out.print(t + " "));
+  }
+
+  public static void ciftSayiLambda(List<Integer> l) {
+    //lambda expression (->arrow function)
+    l.stream().filter(t -> t % 2 == 0).forEach(t -> System.out.print(t + " "));
+  }
+
+  public static void ciftSayiJava(List<Integer> l) {
+    for (Integer w : l) {
+      if (w % 2 == 0) System.out.print(w + " ");
+    }
+  }
+
+  public static void yazdirJava(List<Integer> list) {
+    for (Integer w : list) {
+      System.out.print(w + " ");
+    }
+  }
+
+  public static void yazdirLambda(List<Integer> l) {
+    l.stream().forEach(t -> System.out.print(t + " "));
+  }
+}
+//3) List teki tek sayıların karelerini yazdıran metod oluşturun
+//4) Listedeki tekrarlı elemanları silip tek sayı olanların küplerini yazdıran metod oluştur
+//5) Lİstedeki tekrarlı elemanları silip çift sayı olanların karelerinin toplamını yazdıran metot oluştur
---
+++ src/LambdaDersleri/IkinciGun/Abstract/Sekil.java
@@ -0,0 +1,23 @@
+package LambdaDersleri.IkinciGun.Abstract;
+
+public abstract class Sekil {
+
+  /*
+Şekil diye bir class oluşturalım
+cevre ve alan abstract methodları olsun
+
+uçgen
+    -eşkenar
+    -ikizkenar
+    -çeşitkenar
+
+dikdörtgen
+daire
+
+herbirinin alan ve cevresini hesaplayan kodu yazın
+    */
+
+  public abstract int cevre();
+
+  public abstract int alan();
+}
