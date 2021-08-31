5fb4a3c4b849466e05c8d1b0510529a9dd24b1e0
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-08-31 18:12:45 +0200

    Lambda

--- src/LambdaDersleri/IkinciGun/AbstractIkinciSoru/Runner.java
+++ src/LambdaDersleri/IkinciGun/AbstractIkinciSoru/Runner.java
@@ -1,6 +1,24 @@
 package LambdaDersleri.IkinciGun.AbstractIkinciSoru;
 
+import java.util.Scanner;
+
 public class Runner {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    Musteri musteri = new Musteri();
+    System.out.println(
+      "Ekmeği nereden almak istersiniz : \n1-Market\n2-Halkekmek"
+    );
+    int tercih = scan.nextInt();
+    if (tercih == 1) {
+      musteri.anaAbstract = new Market();
+      musteri.alisveris();
+      System.out.println();
+    } else {
+      musteri.anaAbstract = new HalkEkmek();
+      System.out.println();
+      musteri.alisveris();
+    }
+  }
 }
